8642c2cc18c20d441847f672e2ebf8dbbce7b6ca
C0x41lch0x41 <C0x41lch0x41@users.noreply.github.com>
2022-08-31 16:59:00 -0700

    [tools/stellar-sign] Added capability to verify a TX vs signing it

--- tools/stellar-sign/main.go
+++ tools/stellar-sign/main.go
@@ -1,28 +1,94 @@
 // stellar-sign is a small interactive utility to help you contribute a
-// signature to a transaction envelope.
+// signature to a transaction envelope or verify a transaction.
 //
-// It prompts you for a key
+// It prompts you for a key, public (verify) or private (sign)
 package main
 
 import (
 	"bufio"
+	b64 "encoding/base64"
 	"flag"
 	"fmt"
-	"github.com/stellar/go/keypair"
-	"github.com/stellar/go/network"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
 
 	"github.com/howeyc/gopass"
 	"github.com/stellar/go/txnbuild"
 	"github.com/stellar/go/xdr"
 )
 
+type SignOrVerify struct {
+	verify  bool
+	privKey *keypair.Full
+	pubKey  *keypair.FromAddress
+}
+
+func (router *SignOrVerify) setKey(input string) {
+	var err error
+	if router.verify {
+		inputPubKey, err := keypair.Parse(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		router.pubKey = inputPubKey.FromAddress()
+	} else {
+		router.privKey, err = keypair.ParseFull(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func (router *SignOrVerify) do(gentx *txnbuild.GenericTransaction, networkPassphrase string) (string, error) {
+	var newEnv string
+	var err error
+	if router.verify {
+		var signature []byte
+		var body []byte
+		var gentxEnv xdr.TransactionEnvelope
+		gentxEnv, err = gentx.ToXDR()
+		if err != nil {
+			log.Fatal(err)
+		}
+		signature = gentxEnv.Signatures()[0].Signature
+		gentxHash, _ := gentx.Hash(networkPassphrase)
+		body = gentxHash[:]
+		err = router.pubKey.Verify(body, signature)
+	} else {
+		if tx, ok := gentx.Transaction(); ok {
+			tx, err = tx.Sign(networkPassphrase, router.privKey)
+			if err != nil {
+				log.Fatal(err)
+			}
+			newEnv, err = tx.Base64()
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			tx, _ := gentx.FeeBump()
+			tx, err = tx.Sign(networkPassphrase, router.privKey)
+			if err != nil {
+				log.Fatal(err)
+			}
+			newEnv, err = tx.Base64()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	return newEnv, err
+}
+
 var in *bufio.Reader
 
 var infile = flag.String("infile", "", "transaction envelope")
+var verify = flag.Bool("verify", false, "Verify the transaction instead of signing")
+var testnet = flag.Bool("test", false, "Testnet")
 
 func main() {
 	flag.Parse()
@@ -70,6 +136,9 @@ func main() {
 	fmt.Printf("  source: %s\n", sourceAccount.Address())
 	fmt.Printf("  ops: %d\n", len(txe.Operations()))
 	fmt.Printf("  sigs: %d\n", len(txe.Signatures()))
+	for _, signature := range txe.Signatures() {
+		fmt.Printf("    %s\n", b64.StdEncoding.EncodeToString(signature.Signature))
+	}
 	if txe.IsFeeBump() {
 		fmt.Printf("  fee bump sigs: %d\n", len(txe.FeeBumpSignatures()))
 	}
@@ -77,49 +146,37 @@ func main() {
 
 	// TODO: add operation details
 
-	// read seed
-	seed, err := readLine("Enter seed: ", true)
+	// read seed/public key
+	key, err := readLine("Enter key", true)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// sign the transaction
-	kp, err := keypair.ParseFull(seed)
-	if err != nil {
-		log.Fatal(err)
-	}
+	flowRouter := &SignOrVerify{verify: *verify}
+	flowRouter.setKey(key)
 
 	parsed, err := txnbuild.TransactionFromXDR(env)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var newEnv string
-	if tx, ok := parsed.Transaction(); ok {
-		tx, err = tx.Sign(network.PublicNetworkPassphrase, kp)
+	passPhrase := network.PublicNetworkPassphrase
+	if *testnet {
+		passPhrase = network.TestNetworkPassphrase
+	}
+	newEnv, err := flowRouter.do(parsed, passPhrase)
+	if *verify {
 		if err != nil {
-			log.Fatal(err)
-		}
-		newEnv, err = tx.Base64()
-		if err != nil {
-			log.Fatal(err)
+			fmt.Print("\nSignature is INVALID\n")
+		} else {
+			fmt.Print("\nSignature is VALID\n")
 		}
 	} else {
-		tx, _ := parsed.FeeBump()
-		tx, err = tx.Sign(network.PublicNetworkPassphrase, kp)
-		if err != nil {
-			log.Fatal(err)
-		}
-		newEnv, err = tx.Base64()
-		if err != nil {
-			log.Fatal(err)
-		}
+		fmt.Print("\n==== Result ====\n\n")
+		fmt.Print("```\n")
+		fmt.Println(newEnv)
+		fmt.Print("```\n")
 	}
-
-	fmt.Print("\n==== Result ====\n\n")
-	fmt.Print("```\n")
-	fmt.Println(newEnv)
-	fmt.Print("```\n")
 
 }
 
